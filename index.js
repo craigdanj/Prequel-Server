b2ed127a25fb3932c36c549096872a2a9d1f2d81
Craig <craigdanj@gmail.com>
2018-10-20 16:47:30 +0530

    initial server created

---
+++ index.js
@@ -0,0 +1,60 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const tasks = [
+    {
+        name: "test 1",
+        completed: false
+    },
+    {
+        name: "test 2",
+        completed: false
+    },
+    {
+        name: "test 3",
+        completed: false
+    },
+    {
+        name: "test 4",
+        completed: false
+    },
+    {
+        name: "test 5",
+        completed: false
+    }
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Task" type can be used in other type declarations.
+  type Task {
+    name: String
+    completed: Boolean
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  type Query {
+    tasks: [Task]
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve tasks from the "tasks" array above.
+const resolvers = {
+  Query: {
+    tasks: () => tasks,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+
+// This `listen` method launches a web-server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
